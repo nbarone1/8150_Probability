d5d1a00aa05a8b61abb96648ec57fead1d5a6a6b
nbarone1 <47761143+nbarone1@users.noreply.github.com>
2024-03-21 17:36:59 -0400

    fixed eigenvalues

--- HW8/3State.py
+++ HW8/3State.py
@@ -1,9 +1,10 @@
 # Metropolis Hasting Sampling + Extras
 
 from scipy import stats
-import tqdm
+from tqdm import tqdm
 import numpy as np
-from numpy.linalg import eig
+from scipy.linalg import eig
+import click
 
 class metro_hast_chain:
     def __init__(self,N,E,P,PP):
@@ -12,12 +13,14 @@ class metro_hast_chain:
         self.initial_prob = np.array(P)
         self.proposal_prob = np.array(PP)
         self.chain = []
-        self.X = stats.rv_discrete(self.elements,self.initial_prob)
-        self.Y = stats.rv_discrete(self.elements,self.proposal_prob)
-        self.M = self.initial_prob*self.proposal_prob.T
+        self.X = stats.rv_discrete(name="X",values=(self.elements,self.initial_prob))
+        self.Y = stats.rv_discrete(name="Y",values=(self.elements,self.proposal_prob))
+        self.M = np.asmatrix(self.initial_prob*self.proposal_prob.reshape(3,1))
 
     def start(self):
-        self.chain.append(self.X.rvs(1))
+        s = self.X.rvs(1)
+        self.chain.append(s)
+        return s
 
     def advance(self):
         for i in tqdm(range(self.chain_length)):
@@ -29,6 +32,53 @@ class metro_hast_chain:
                 self.chain.append(x0)
 
     def stationary_dist(self):
-        L,V = eig(self.M)
+        L,V = eig(self.M,left = True,right = False)
         l1i = np.argmax(L)
         l1v = L.max()
+        stat_dist = V[:, 0].T/sum(V[:,0])
+        return l1v,stat_dist
+    
+@click.command()
+@click.option(
+    '--num','-n',
+    default=100,
+    type = int,
+    show_default=True,
+    help='Number of itartations in the chain'
+)
+@click.option(
+    '--element','-e',
+    default=[1,2,3],
+    type = list,
+    show_default=True,
+    help='List of elements/states'
+)
+@click.option(
+    '--probability','-p',
+    default=[1/2,1/3,1/6],
+    type=list,
+    show_default=True,
+    help='Probabilities of each element/state'
+)
+@click.option(
+    '--proposal','-pp',
+    default=[.99,.009,.001],
+    type = list,
+    show_default=True,
+    help='Probability of Proposing a state'
+)
+
+def main(num,element,probability,proposal):
+    mhc3 = metro_hast_chain(num,element,probability,proposal)
+    start_state = mhc3.start()
+    print(start_state)
+    mhc3.advance()
+    eigenvalue, stationary = mhc3.stationary_dist()
+    print(stationary)
+    print(eigenvalue)
+    chain = mhc3.chain
+    print(chain)
+
+
+if __name__ == "__main__":
+    main()
