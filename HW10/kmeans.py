836ff191c10394434011acac27bed0b2571b81f2
nbarone1 <47761143+nbarone1@users.noreply.github.com>
2024-04-11 19:15:06 -0400

    batching approach

--- HW10/kmeans.py
+++ HW10/kmeans.py
@@ -15,17 +15,18 @@ import click
 import cupy as cp
 
 class cluster():
-    def __init__(self,maxiter):
+    def __init__(self,epochs,maxiter):
         self.kn = 10
+        self.epoch = epochs
         self.maxiter = maxiter
         self.xtrain, self.ytrain, self.xtest,self.ytest = self.data_prep()
-        self.centers = self.train_fit()
+        self.centers = self.train()
 
     def data_prep(self):
         (X1,Y1), (X2,Y2) = keras.datasets.mnist.load_data()
-        xtrain = cp.array(X1.reshape(60000,784))
+        xtrain = cp.divide(cp.array(X1.reshape(60000,784)),255)
         ytrain = cp.array(Y1)
-        xtest = cp.array(X2.reshape(10000,784))
+        xtest = cp.divide(cp.array(X2.reshape(10000,784)),255)
         ytest = cp.array(Y2)
         assert xtrain.ndim == 2
         assert ytrain.ndim == 1
@@ -34,39 +35,57 @@ class cluster():
 
         return xtrain,ytrain,xtest,ytest
     
-    def fit_step(self,data,pred,centers):
-        dist = cp.linalg.norm(data[:, None, :] - centers[None, :, :],axis = 2)
-        new_pred = cp.argmin(dist, axis = 1)
-
-        pred = new_pred
+    def minibatch(self,data):
+        mini = cp.random.choice(60000,1024,replace=False)
+        d2 = data[mini]
+        return d2
+    
+    def fit_step(self,data,centers):
+        minib = self.minibatch(data)
+        pred = cp.zeros(len(minib))
+        dist = cp.linalg.norm(minib[:, None, :] - centers[None, :, :],axis = 2)
+        pred = cp.argmin(dist, axis = 1)
 
         c = cp.arange(self.kn)
         mask = pred == c[:,None]
-        sums = cp.where(mask[:, :, None], data, 0).sum(axis = 1)
+        sums = cp.where(mask[:, :, None], minib, 0).sum(axis = 1)
         counts = cp.count_nonzero(mask, axis = 1).reshape((self.kn,1))
         centers = sums/counts
 
-        return pred,centers   
+        return centers
+    
+    def pred_step(self,data,centers):
+        dist = cp.linalg.norm(data[:, None, :] - centers[None, :, :],axis = 2)
+        pred = cp.argmin(dist, axis = 1)
+        return pred
 
-    def train_fit(self):
-        pred = cp.zeros(60000)
-        initial_center = cp.random.choice(60000,self.kn,replace=False)
-        centers = self.xtrain[initial_center]
-        change = cp.zeros(self.maxiter)
-        acc = cp.zeros(self.maxiter)
+    def batch_fit(self,pred,centers):
+        ocenters = centers
 
-        for i in tqdm(range(self.maxiter)):
-            new_pred, new_centers = self.fit_step(self.xtrain,pred,centers)
+        for i in range(self.maxiter):
+            new_centers = self.fit_step(self.xtrain,centers)
+            new_pred = self.pred_step(self.xtrain,new_centers)
 
             if cp.all(new_pred == pred):
-                acc[i-1::] = acc[i-2]
-                change[i-1::] = 0
-                break
-            change[i-1] = cp.linalg.norm(new_pred - pred)
+                change = cp.linalg.norm(ocenters - centers)
+                acc = cp.divide(cp.sum(cp.equal(self.ytrain,pred)),len(self.ytrain))
+                break            
             pred = new_pred
             centers = new_centers
 
-            acc[i-1] = cp.sum(cp.count_nonzero(self.ytrain == pred))/60000
+        change = cp.linalg.norm(ocenters - centers)
+        acc = cp.divide(cp.sum(cp.equal(self.ytrain,pred)),len(self.ytrain))
+
+        return change, acc, pred,centers
+    
+    def train(self):
+        pred = cp.zeros(60000)
+        initial_center = cp.random.choice(60000,self.kn,replace=False)
+        centers = self.xtrain[initial_center]
+        acc = cp.zeros(self.epoch)
+        change = cp.zeros(self.epoch)
+        for i in tqdm(range(self.epoch)):
+            change[i],acc[i],pred,centers = self.batch_fit(pred,centers)
 
         fig, ax1 = plt.subplots()
         
@@ -79,20 +98,35 @@ class cluster():
         ax1.legend(loc='upper right', bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True)
         ax2.legend(loc='upper left', bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True)
         plt.savefig('accuracy_change.png')
-
         return centers
+    
+    def test_step(self):
+        fpred = self.pred_step(self.xtest,self.centers)
+        acc = cp.divide(cp.sum(cp.equal(self.ytest,fpred)),len(self.ytest))
+        acc *= 100
+        print("Accuracy on Testing Data is %5.2f %" % (acc))
+
+def run(cluster):
+    cluster.test_step()
+
 
 @click.command()
 @click.option(
+    '--epochs','-e',
+    default=50,
+    show_default=True,
+    help='Max Iterations'
+)
+@click.option(
     '--maxiter','-m',
-    default=100,
+    default=60,
     show_default=True,
     help='Max Iterations'
 )
 
-def main(maxiter):
-    c1 = cluster(maxiter)
-    # run(c1)
+def main(epochs,maxiter):
+    c1 = cluster(epochs,maxiter)
+    run(c1)
 
 if __name__ == "__main__":
     plt.ion()
