b66aceaec1aa0aa5dedb5da1a2755bdff087c14f
nbarone1 <47761143+nbarone1@users.noreply.github.com>
2024-04-10 21:42:07 -0400

    first success

--- HW10/kmeans.py
+++ HW10/kmeans.py
@@ -1,7 +1,10 @@
 # K-Means from Scratch
 
-# Using scikit-learn to compare
-# Using GPU to run code
+# Using GPU to run code via cuPy
+
+import os
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as manimation
@@ -13,91 +16,85 @@ import click
 import cupy as cp
 
 class cluster():
-    def __init__(self,kn,maxiter):
-        self.kn = kn
+    def __init__(self,maxiter):
+        self.kn = 10
         self.maxiter = maxiter
-        self.train, self.test = self.data_prep()
-        self.size = len(self.data)
-        self.pred, self.centers = self.fit()
+        self.xtrain, self.ytrain, self.xtest,self.ytest = self.data_prep()
+        self.centers = self.train_fit()
 
     def data_prep(self):
         (X1,Y1), (X2,Y2) = keras.datasets.mnist.load_data()
-        train = (X1[:5000].reshape(5000,784),Y1[:5000])
-        test = (X2[:1000].reshape(1000,784), Y2[:1000])
+        xtrain = cp.array(X1.reshape(60000,784))
+        ytrain = cp.array(Y1)
+        xtest = cp.array(X2.reshape(10000,784))
+        ytest = cp.array(Y2)
+        assert xtrain.ndim == 2
+        assert ytrain.ndim == 1
+        assert xtest.ndim == 2
+        assert ytest.ndim == 1
 
-        return train,test
+        return xtrain,ytrain,xtest,ytest
     
-    def fit_step(self,pred,centers):
-        dist = cp.linalg.norm(self.data[: None, :] - centers[None, :, :],axis = 2)
+    def fit_step(self,data,pred,centers):
+        dist = cp.linalg.norm(data[:, None, :] - centers[None, :, :],axis = 2)
         new_pred = cp.argmin(dist, axis = 1)
 
         pred = new_pred
 
         c = cp.arange(self.kn)
         mask = pred == c[:,None]
-        sums = cp.where(mask[:, :, None], self.data, 0).sum(axis = 1)
+        sums = cp.where(mask[:, :, None], data, 0).sum(axis = 1)
         counts = cp.count_nonzero(mask, axis = 1).reshape((self.kn,1))
         centers = sums/counts 
 
         return pred,centers   
 
-    def fit(self):
-        pred = cp.zeros(self.size)
-        initial_center = cp.random.choice(self.size,self.kn,replace=False)
-        centers = self.data[initial_center]
+    def train_fit(self):
+        pred = cp.zeros(60000)
+        initial_center = cp.random.choice(60000,self.kn,replace=False)
+        centers = self.xtrain[initial_center]
+        change = cp.zeros(self.maxiter)
+        acc = cp.zeros(self.maxiter)
 
-        FFMpegWriter = manimation.writers['ffmpeg']
-        writer = FFMpegWriter(fps=10)
+        for i in tqdm(range(self.maxiter)):
+            new_pred, new_centers = self.fit_step(self.xtrain,pred,centers)
 
-        fig = plt.figure()
+            if cp.all(new_pred == pred):
+                acc[i-1::] = acc[i-2]
+                change[i-1::] = 0
+                break
+            change[i-1] = cp.sum(cp.subtract(pred,new_pred))
+            pred = new_pred
+            centers = new_centers
 
+            acc[i-1] = (cp.count_nonzero(pred == self.ytrain)/60000)*100
 
-        # NEED THIS TO BE 28x28 IMAGE OF THE MEANS
-        with writer.saving(fig, "mnist_mask.mp4", 50):
-
-            for i in tqdm(range(self.maxiter)):
-                new_pred, new_centers = self.fit_step(pred,centers)
-
-                if cp.all(new_pred == pred):
-                    break
-                pred = new_pred
-                centers = new_centers
-
-                # Run Iterations and record images
-                if i % 1000 == 0:
-                    for k in range(self.kn):
-                        labels = self.data[pred == k]
-                        img = plt.scatter(labels[:, 0], labels[:, 1], c=cp.random.rand(3))
-                    img = plt.scatter(centers[:, 0], centers[:, 1], s=120, marker='s', facecolors='y',edgecolors='k')
-                    writer.grab_frame()
-                    img.remove()
-                    
+        fig, ax1 = plt.subplots()
         
-        plt.close('all')
+        ax2 = ax1.twinx()
+        ax1.plot(cp.asnumpy(acc), 'r-',)
+        ax2.plot(cp.asnumpy(change),'g-')
+        ax1.set_xlabel('Iterations')
+        ax1.set_ylabel('Accuracy %')
+        ax2.set_ylabel('Change in Centroids')
+        plt.savefig('accuracy_change.png')
+
+        return centers
     
-        return pred, centers
-    
-def run(cluster):
-    pred, center = cluster.fit()
-    return
+# def run(cluster):
+    # return
 
 @click.command()
 @click.option(
-    '--kn','-k',
-    default=10,
-    show_default=True,
-    help='Number of Clusters'
-)
-@click.option(
     '--maxiter','-m',
-    default=10000,
+    default=50,
     show_default=True,
     help='Max Iterations'
 )
 
-def main(kn,maxiter,):
-    c1 = cluster(kn,maxiter)
-    run(c1)
+def main(maxiter):
+    c1 = cluster(maxiter)
+    # run(c1)
 
 if __name__ == "__main__":
     plt.ion()
