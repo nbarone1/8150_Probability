9f7f892d7b3d34308e9615a3c9eb4589c8b8ae06
nbarone1 <47761143+nbarone1@users.noreply.github.com>
2024-05-10 22:01:16 -0500

    fuck this

---
+++ Final/cheat_casino.py
@@ -0,0 +1,14 @@
+# Cheating Casino
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+size = 200
+beta = 1/2
+
+system = np.random.choice([-1,1],size)
+
+# The next step is to generate Y values based on the randomly formed system
+# The idea behind a beta of 1/2 is based on research done on 1D Ising systems of electromagnetism.
+# The rest of this problem I did not get to work out thoroughly.
+# There were issues with travel and attending my sister's graduation that cut into time spent on problem 3.
